b6589a399beaa682053e4dd757728d3185d584be
Timothy Naylor <tim@phatpenguin.com>
2015-11-18 16:44:00 -0700

    Initial Commit

---
+++ testingExercises/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testingExercises
+//
+//  Created by Timothy Naylor on 11/18/15.
+//  Copyright © 2015 phatpenguin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
